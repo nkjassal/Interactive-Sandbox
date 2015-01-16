bf89337ddbb6617b6486dbd016ba367324353722
nkjassal <Neil@NJ.wv.cc.cmu.edu>
2015-01-16 02:05:09 -0500

    added servo controller with all servo sweep

--- Servos/servo_calibration/servo_calibration.ino
+++ Servos/servo_calibration/servo_calibration.ino
@@ -4,12 +4,36 @@ Servo servo;
 
 /***
 /* servo  0  1  2  3  4  5  6  7  8
-/* weight 12       12       6
+/* weight 
 
 /*5 volts, expect 5.75 for sand */
 /* low and high positions for all 9 arduinos, labeled and corresponding to the array index */
-int servo_low[] = {145, 145, 0, 85, 40, 0, 155, 0, 130};
-int servo_high[] = {180, 180, 0, 115, 70, 0, 180, 0, 170};
+int servo_low[] = {0, 0, 150, 4, 0, 143, 8, 154, 150};
+int servo_high[] = {0, 0, 177, 25, 0, 170, 25, 177, 177};
+
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  servo.attach(9);
+  servo.write(8);
+  /*delay(3000);
+  servo.write(10);*/
+  //delay(1000);
+  //servo.write(175);
+  
+ //sweep_servo(8);
+
+
+}
+
+
+void loop()
+{
+  sweep_servo(3);
+}
+
 
 void sweep_servo(int num)
 {
@@ -27,27 +51,4 @@ void sweep_servo(int num)
     servo.write(height);
     delay(20);
   }
-}
-
-void setup()
-{
-  Serial.begin(9600);
-  
-  servo.attach(9);
-  servo.write(180);
-  /*delay(1000);
-  servo.write(140);
-  delay(1000);
-  servo.write(175);*/
-  
- sweep_servo(6);
-
-
-}
-
-
-
-void loop()
-{
-  sweep_servo(6);
 }
