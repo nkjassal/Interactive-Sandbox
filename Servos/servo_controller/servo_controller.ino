a9fed05b04bc573971e7633f6b5afe8eeda4a154
nkjassal <Neil@NJ.wv.cc.cmu.edu>
2015-01-16 04:54:22 -0500

    its 5am

--- Servos/servo_controller/servo_controller.ino
+++ Servos/servo_controller/servo_controller.ino
@@ -5,7 +5,7 @@ Servo servos[] = {s0, s1, s2, s3, s4, s5, s6, s7, s8};
 
 /* for each servo (corresponding to the index), the arduino pin
  * at which it's attached */
-int pins[] = {0};
+int pins[] = {2,3,4,5,6,7,8,9,10};
 
 /* high and low values platform corresponding to each servo */
 int servo_low[] = {130, 140, 150, 4, 60, 143, 8, 154, 150};
@@ -13,8 +13,8 @@ int servo_high[] = {165, 175, 177, 25, 95, 170, 25, 177, 177};
 int servo_pos[9];
 int servo_vel[] = {5,1,2,6,3,7,2,3,4};
 
-void sweep_all();
-void sweep_servo(int servo_num);
+//void sweep_all();
+//void sweep_servo(int servo_num);
 
 
 void setup()
@@ -23,14 +23,22 @@ void setup()
   Serial.begin(9600);
   for(int i = 0; i < 9; i++) {
     servos[i].attach(pins[i]);
-    servo_pos[9] = (int)random(0,360);
+    servos[i].write(servo_high[i]);
+  // servo_pos[9] = (int)random(0,360);
   }
+  
+  //s0.attach(pins[0]);
+
+  
+  
 }
 
 void loop()
 {
-  for(int i = 0; i < 9; i++) {
-    float servo_pos_rads = servo_pos[9]/180*3.1415926535
+  sweep_servo(random(8));
+  delay(500);
+  /*for(int i = 0; i < 9; i++) {
+    float servo_pos_rads = servo_pos[9]/180*3.1415926535;
     
     double nextpos = (sin(servo_pos_rads) + 1)/2;
     int lo = servo_low[i];
@@ -40,7 +48,7 @@ void loop()
     servos[i].write(actualpos);
     
     servo_pos[i] = (servo_pos[i] + servo_vel[i]) % 360;
-  }
+  }*/
 }
 
 /* sweep_all - moves all servos up down to their low height 
@@ -50,7 +58,6 @@ void loop()
  * Works simultaneously - all servos will move one together,
  * one degree at a time
  */
-/*
 void sweep_all()
 {
   int heights[9];
@@ -79,7 +86,11 @@ void sweep_all()
     for(int j = 0; j < 9; j++)
     {
       if(heights[j] != servo_low[j])
+      {
         heights[j]--;
+        servos[j].write(heights[j]);
+        delay(100);
+      }
     }  
   }
   
@@ -103,13 +114,18 @@ void sweep_all()
     for(int j = 0; j < 9; j++)
     {
       if(heights[j] != servo_high[j])
+      {
         heights[j]++;
+        servos[j].write(heights[j]);
+        delay(100);
+      }
+
     }  
   }
   
   Serial.println("sweep of all servos complete");
 }
-*/
+
 /* sweep_servo - moves single servo down to lowest height
  * and back up to max height. works regardless of starting
  * position of the servo
